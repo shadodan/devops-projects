be6fc6ec011e75dc08eae10c8ad251da361e0708
matheus-dr <m47heusdr@protonmail.com>
2022-11-07 07:07:54 -0300

    Adding ci cd with docker ECS

---
+++ jenkins-intro/docker-ci-cd/userdata/jenkins-server-setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+## Installing Jenkins ##
+sudo apt update
+sudo apt install openjdk-11-jdk maven -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+## Installing Docker ##
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+## Configuring connection between Jenkins and Docker ##
+sudo usermod -a -G docker jenkins
+
+## Installing AWS CLI ##
+sudo apt-get install awscli -y
+
+## Rebooting machine ##
+reboot
